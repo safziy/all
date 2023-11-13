f8fad873710ce89301714ce61c99109e17b2aecc
safziyzhang <safziyzhang@tencent.com>
2023-11-13 16:36:42 +0800

    模拟登录

---
+++ database/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `t_user`(`id`, `name`) VALUES (1,'default');
--- src/main/java/com/safziy/controller/UserController.java
+++ src/main/java/com/safziy/controller/UserController.java
@@ -1,19 +1,12 @@
 package com.safziy.controller;
 
-import com.mybatisflex.core.paginate.Page;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.safziy.constants.WebConstants;
+import com.safziy.controller.base.Resp;
+import com.safziy.controller.base.WebSupport;
+import com.safziy.utils.WebUtil;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.safziy.entity.User;
-import com.safziy.service.UserService;
 import org.springframework.web.bind.annotation.RestController;
-import java.io.Serializable;
-import java.util.List;
 
 /**
  *  控制层。
@@ -23,74 +16,12 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements WebSupport {
 
-    @Autowired
-    private UserService userService;
-
-    /**
-     * 添加。
-     *
-     * @param user 
-     * @return {@code true} 添加成功，{@code false} 添加失败
-     */
-    @PostMapping("save")
-    public boolean save(@RequestBody User user) {
-        return userService.save(user);
-    }
-
-    /**
-     * 根据主键删除。
-     *
-     * @param id 主键
-     * @return {@code true} 删除成功，{@code false} 删除失败
-     */
-    @DeleteMapping("remove/{id}")
-    public boolean remove(@PathVariable Serializable id) {
-        return userService.removeById(id);
-    }
-
-    /**
-     * 根据主键更新。
-     *
-     * @param user 
-     * @return {@code true} 更新成功，{@code false} 更新失败
-     */
-    @PutMapping("update")
-    public boolean update(@RequestBody User user) {
-        return userService.updateById(user);
-    }
-
-    /**
-     * 查询所有。
-     *
-     * @return 所有数据
-     */
-    @GetMapping("list")
-    public List<User> list() {
-        return userService.list();
-    }
-
-    /**
-     * 根据主键获取详细信息。
-     *
-     * @param id 主键
-     * @return 详情
-     */
-    @GetMapping("getInfo/{id}")
-    public User getInfo(@PathVariable Serializable id) {
-        return userService.getById(id);
-    }
-
-    /**
-     * 分页查询。
-     *
-     * @param page 分页对象
-     * @return 分页对象
-     */
-    @GetMapping("page")
-    public Page<User> page(Page<User> page) {
-        return userService.page(page);
+    @PostMapping("mockLogin")
+    public Resp<Object> mockLogin() {
+        WebUtil.getSession().setAttribute(WebConstants.LoginUserId, 1);
+        return success(null);
     }
 
 }
